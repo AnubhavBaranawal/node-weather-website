a5abe13b8c1e35f9b964625ad19d97b90fc1996f
AnubhavBaranawal <anubhavbaranawal1@gmail.com>
2020-06-19 15:32:07 +0530

    Data added to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback("Unable to find location",undefined)
         }else{
-            callback(undefined,'It is currently ' + body.current.temperature + ' degrees out. Chance of ' + body.current.precip + '% rain.')
+            callback(undefined,'It is currently ' + body.current.temperature + ' degrees out. Chance of ' + body.current.precip + '% rain. Present humidity is ' + body.current.humidity + ', wind speed is ' + body.current.wind_speed + 'km/h and cloud cover is ' + body.current.cloudcover + '%.')
         }
     })
 }
